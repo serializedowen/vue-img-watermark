a547e1b228799054babed4d0cf05ac0092d68f05
serializedowen <wjh199455@gmail.com>
2020-08-08 02:09:55 +0800

    demo

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import plugin, { setScopedConfig } from "../src/index";
 import img from "./example.jpg";
-
+import img2 from "./ex2.jpg";
 Vue.use(plugin);
 
 console.log(img);
@@ -9,16 +9,16 @@ const ele = new Vue({
   components: {
     seperate: {
       beforeCreate() {
-        setScopedConfig({ mode: "topleft" }, this);
+        setScopedConfig({ mode: "topright" }, this);
       },
       mounted() {
-        setScopedConfig({ mode: "bottomleft", rotate: 20 }, this);
+        // setScopedConfig({ rotate: 20 }, this);
       },
       render(h) {
         return (
           <div>
-            <img v-watermark={{ bbb: "aaaa" }} src={"/" + img}></img>
-            {/* <img v-watermark src="./example.jpg"></img> */}
+            <img v-watermark src={"/" + img2}></img>
+            <img v-watermark src={"/" + img}></img>
           </div>
         );
       },
@@ -28,8 +28,8 @@ const ele = new Vue({
     return (
       <div>
         <seperate></seperate>
-        <img v-watermark={{ aaa: "bbb" }} src={"/" + img}></img>
-        {/* <img v-watermark src="./example.jpg"></img> */}
+        <img v-watermark={{ content: "bbb" }} src={"/" + img}></img>
+        <img v-watermark={{ content: "abee" }} src={"/" + img}></img>
       </div>
     );
   },
