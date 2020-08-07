5cf2e8fc1d972a205998ef5af8ce3632e9faf808
serializedowen <wjh199455@gmail.com>
2020-08-08 02:09:39 +0800

    active development

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { VueConstructor } from "vue";
+import { DirectiveBinding } from "vue/types/options";
+import { VNode } from "vue/types/umd";
+import selectStrategy, { registerCustomStrategy } from "./selectStrategy";
 
 let canvas: HTMLCanvasElement;
 
@@ -7,44 +10,65 @@ export type displayMode =
   | "bottomright"
   | "topleft"
   | "topright"
-  | "tilted";
+  | "center"
+  | "fill";
 
-export type optionConfig = {
+registerCustomStrategy("fill", (ctx, options) => {
+  ctx.textAlign = "center";
+  const width = ctx.canvas.width;
+  const height = ctx.canvas.height;
+  ctx.translate(width / 2, -height / 2);
+  ctx.rotate((options.rotate * Math.PI) / 180);
+  let x = 20;
+  let y = 20;
+  let addingX = true;
+  while (y < height * 2) {
+    ctx.fillText(options.content, x, y);
+
+    if (x > width * 2) {
+      x = 20;
+      addingX = false;
+    }
+
+    if (addingX) x += 80;
+    else {
+      y += 80;
+      addingX = true;
+    }
+  }
+});
+
+export type OptionConfig = {
   mode: displayMode;
-  textAlign: CanvasTextAlign;
   textBaseline: CanvasTextBaseline;
   fillStyle: string | CanvasGradient | CanvasPattern;
   content: string;
   font: string;
   rotate: number;
-  zIndex: number;
 };
 
-const defaultOptions: optionConfig = Object.freeze({
-  mode: "tilted",
-  textAlign: "center",
+const defaultOptions: OptionConfig = Object.freeze({
+  mode: "fill",
   textBaseline: "middle",
   font: "15px Arial",
   fillStyle: "rgba(184, 184, 184, 0.8)",
   content: "请勿外传",
   rotate: 30,
-  zIndex: 1000,
 });
 
-const globalOptions: optionConfig = { ...defaultOptions };
+let globalOptions: OptionConfig = { ...defaultOptions };
+const scopedConfigMap: WeakMap<Vue, OptionConfig> = new WeakMap();
 
-// const scopedConfigMap: {
-//   [k in number]: optionConfig;
-// } = {};
-
-const scopedConfigMap: WeakMap<Vue, optionConfig> = new WeakMap();
-
-export const setScopedConfig = (opts: Partial<optionConfig>, vm: Vue) => {
+export const setScopedConfig = (opts: Partial<OptionConfig>, vm: Vue) => {
   if (!scopedConfigMap.has(vm)) {
     scopedConfigMap.set(vm, { ...defaultOptions, ...opts });
   } else {
     scopedConfigMap.set(vm, { ...scopedConfigMap.get(vm), ...opts });
   }
+};
+
+export const setGlobalConfig = (opts: Partial<OptionConfig>) => {
+  globalOptions = { ...globalOptions, ...opts };
 };
 
 const getCanvas = () => {
@@ -53,75 +77,64 @@ const getCanvas = () => {
     // canvas.style.display = "none";
     // canvas.style.position = "absolute";
     // canvas.style.zIndex = "-9999";
-    document.body.append(canvas);
+    // document.body.append(canvas);
   }
   return canvas;
-};
-
-export const setCanvasAttr = (opts: Partial<optionConfig> = {}) => {
-  Object.keys(opts).map((key) => {
-    // getCanvas().setAttribute(key, options[key]);
-  });
-
-  return getCanvas();
 };
 
 const vueImgWatermark = {
   install: (Vue: VueConstructor) => {
     Vue.directive("watermark", {
-      inserted(element: HTMLImageElement, binding, VNode, oldVNode) {
-        let marked = false;
-        let options: optionConfig;
+      bind(
+        element: HTMLImageElement,
+        binding: DirectiveBinding,
+        VNode: VNode
+        // oldVNode: VNode
+      ) {
+        let options: OptionConfig;
+
+        const bindingOptions: Partial<OptionConfig> = binding.value;
 
         if (scopedConfigMap.has(VNode.context)) {
-          options = scopedConfigMap.get(VNode.context);
+          options = {
+            ...scopedConfigMap.get(VNode.context),
+            ...bindingOptions,
+          };
         } else {
-          options = globalOptions;
+          options = { ...globalOptions, ...bindingOptions };
         }
 
-        element.addEventListener("load", () => {
-          if (!marked) {
-            marked = true;
+        function loader() {
+          element.removeEventListener("load", loader);
+          element.setAttribute("crossorigin", "anonymous");
+          const { width, height } = element;
+          const ctx = getCanvas().getContext("2d");
 
-            element.setAttribute("crossorigin", "anonymous");
-            const { width, height } = element;
-            const ctx = getCanvas().getContext("2d");
+          const {
+            textBaseline,
+            fillStyle,
+            content,
+            rotate,
+            font,
+            mode,
+          } = options;
+
+          setTimeout(() => {
             getCanvas().width = width;
             getCanvas().height = height;
+            ctx.clearRect(0, 0, width, height);
+            ctx.drawImage(element, 0, 0);
 
-            const {
-              textAlign,
-              textBaseline,
-              fillStyle,
-              content,
-              rotate,
-              font,
-              mode,
-            } = options;
+            ctx.textBaseline = textBaseline;
+            ctx.font = font;
+            ctx.fillStyle = fillStyle;
 
-            console.log(binding);
-            setTimeout(() => {
-              ctx.drawImage(element, 0, 0, width, height, 0, 0, width, height);
-              ctx.textAlign = textAlign;
-              ctx.textBaseline = textBaseline;
+            selectStrategy(mode)(ctx, options);
+            element.src = ctx.canvas.toDataURL();
+          }, 200);
+        }
 
-              ctx.font = font;
-              ctx.fillStyle = fillStyle;
-
-              switch (mode) {
-                case "bottomleft": {
-                }
-                case "bottomright": {
-                }
-              }
-
-              ctx.rotate((Math.PI / 180) * rotate);
-              ctx.fillText(content, width / 2, height / 2);
-
-              element.src = ctx.canvas.toDataURL();
-            }, 500);
-          }
-        });
+        element.addEventListener("load", loader);
       },
     });
   },
