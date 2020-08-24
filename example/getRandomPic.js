b3474503ae9acebee7efae094136b221aba02f5d
serializedowen <wjh199455@gmail.com>
2020-08-24 11:42:20 +0800

    optimize: make sure consecutive picture is unique

--- example/getRandomPic.js
+++ example/getRandomPic.js
@@ -1,11 +1,16 @@
 const cache = [];
+let lastInd = null;
+
 function importAll(r) {
   r.keys().forEach((key) => cache.push(r(key).default));
 }
 importAll(require.context("./gallery", false, /\.jpg$/));
 
 const getRandomPic = () => {
-  const ind = Math.floor(Math.random() * cache.length);
+  let ind = lastInd;
+  while (ind == lastInd) ind = Math.floor(Math.random() * cache.length);
+
+  lastInd = ind;
   return cache[ind];
 };
 
